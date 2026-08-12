f448d00c83ac819c3aa5b5d0d94ad2e2b55a5486
mausumi_25 <mausumirautaray2002@gmail.com>
2026-08-13 03:22:32 +0530

    Connect frontend to deployed backend

--- frontend/api.js
+++ frontend/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_URL =
-  process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000';
+  process.env.EXPO_PUBLIC_API_URL ||
+  'https://sprinter-analysis-backend.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
